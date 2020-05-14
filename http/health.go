2dd7acc026fd762971407f9b3e19de0c793b647c
James Warren <james@thisissoon.com>
2020-05-14 18:05:41 +0100

    feat(http): Healthcheck serving status
    Marshal serving status in healthcheck response.

--- http/health.go
+++ http/health.go
@@ -3,6 +3,8 @@ package http
 import (
 	"encoding/json"
 	"net/http"
+
+	"github.com/rs/zerolog"
 )
 
 // Health returns a handler for healthcheck requests
@@ -10,17 +12,27 @@ func (s *Server) Health(app, version string) http.Handler {
 	type healthResponse struct {
 		App     string `json:"app"`
 		Version string `json:"version"`
+		Serving bool   `json:"serving"`
 	}
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log := zerolog.Ctx(r.Context())
+		if log == nil {
+			log = &s.log
+		}
 		b, _ := json.Marshal(healthResponse{
 			App:     app,
 			Version: version,
+			Serving: s.Running,
 		})
 		w.Header().Set("Content-Type", "application/json")
-		w.WriteHeader(http.StatusOK)
+		if s.Running {
+			w.WriteHeader(http.StatusOK)
+		} else {
+			w.WriteHeader(http.StatusServiceUnavailable)
+		}
 		_, err := w.Write(b)
 		if err != nil {
-			s.log.Error().Err(err).Msg("error writing to response")
+			log.Error().Err(err).Msg("error writing to response")
 		}
 	})
 }
