0f86e870b812d73279caf3ff3332f3dfe475b217
Bradley Chatha <brad@thisissoon.com>
2023-08-21 12:52:52 +0100

    feat(grpc): allow specifying additional configuration options on dial

--- grpc/client.go
+++ grpc/client.go
@@ -8,13 +8,17 @@ import (
 )
 
 // NewClient constructs a grpc client connection
-func NewClient(server string) (*grpc.ClientConn, error) {
+func NewClient(server string, grpcOpts ...grpc.DialOption) (*grpc.ClientConn, error) {
+	grpcOpts = append(grpcOpts,
+		// For backwards compatibility, keep these previous, hardcoded options
+		grpc.WithBlock(),
+		grpc.WithInsecure(), // Note: Deprecated in newer versions for grpc.WithTransportCredentials(insecure.NewCredentials())
+	)
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second*5)
 	defer cancel()
 	return grpc.DialContext(
 		ctx,
 		server,
-		grpc.WithInsecure(),
-		grpc.WithBlock(),
+		grpcOpts...,
 	)
 }
